7b1a5101f2259ca06750afe5f92a93ad52070fed
Timofey Zajtsev <misterbobot@yandex.ru>
2021-04-23 23:06:26 +0300

    fixes

--- app/src/lib/pages/workspacePage.tsx
+++ app/src/lib/pages/workspacePage.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { WorkSpaceRoot } from "../ui/templates/workSpaceRoot";
 
-const checkPermissions = () => {
-    const permissions = navigator.mediaDevices.getUserMedia({audio: true, video: false})
-    permissions.then((stream) => {
-      console.log("premmisions");
-    })
-    .catch((err) => {
-        console.log("premmisions error");
-      console.log(`${err.name} : ${err.message}`)
-    });
-  }
+
 
 export const WorkSpacePage : React.FC = () => {
     
-    checkPermissions()
-
     return <>
-    
+
         <WorkSpaceRoot>
             
         </WorkSpaceRoot>
