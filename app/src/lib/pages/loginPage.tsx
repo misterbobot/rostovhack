3fff6dedec543913a92d1268959bee65576e6429
Timofey Zajtsev <misterbobot@yandex.ru>
2021-04-23 23:42:43 +0300

    fixes

--- app/src/lib/pages/loginPage.tsx
+++ app/src/lib/pages/loginPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useSpeechRecognition } from 'react-speech-recognition';
 import { LoginPage } from '../ui/templates/loginPage'
 
 
 export const loginPage: React.FC = () => {
+
+    const { transcript, resetTranscript } = useSpeechRecognition();
 
     return <>
 
