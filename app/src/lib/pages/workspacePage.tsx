1a59f4dc4391cd6f6aba650233525542300ebcbb
Timofey Zajtsev <misterbobot@yandex.ru>
2021-04-23 23:26:50 +0300

    d

--- app/src/lib/pages/workspacePage.tsx
+++ app/src/lib/pages/workspacePage.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { WorkSpaceRoot } from "../ui/templates/workSpaceRoot";
 
-
-
 export const WorkSpacePage : React.FC = () => {
     
     return <>
-
         <WorkSpaceRoot>
             
         </WorkSpaceRoot>
